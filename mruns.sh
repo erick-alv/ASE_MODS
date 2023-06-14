bdf1d5700c1fbb0709b42465e4b3f4acbd24bd1c
Erick <ga74kob@mytum.de>
2023-06-14 09:01:30 +0200

    configs

--- mruns.sh
+++ mruns.sh
@@ -1,6 +1,5 @@
 export LD_LIBRARY_PATH=/home/erick/anaconda3/envs/rlgpu_ase/lib:$LD_LIBRARY_PATH
 
-python ase/run.py --task HumanoidImitationTrack --cfg_env ase/data/cfg/small_humanoid_imitation_vrh_rewPenaltyAndReach.yaml --cfg_train ase/data/cfg/train/rlg/small_common_ppo_humanoid.yaml --motion_file ase/data/motions/cmu_motions_retargeted/180/locomotion/07_01_amp.npy --headless
+python ase/run.py --task HumanoidImitationTrack --cfg_env ase/data/cfg/humanoid_imitation_vrh_rewPenaltyAndReach.yaml --cfg_train ase/data/cfg/train/rlg/common_ppo_humanoid.yaml --motion_file ase/data/motions/dataset_questsimreduced_train.yaml --headless
 
-python ase/run.py --task HumanoidImitationTrack --cfg_env ase/data/cfg/small_humanoid_imitation_vrh_rewPenaltyAndReach_prerew.yaml --cfg_train ase/data/cfg/train/rlg/small_common_ppo_humanoid.yaml --motion_file ase/data/motions/cmu_motions_retargeted/180/locomotion/07_01_amp.npy --headless
 
